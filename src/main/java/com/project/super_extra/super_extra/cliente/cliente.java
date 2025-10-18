12f2ff5923ebb8c2d89fd716e4ac9712c19b5391
David-BarraganP <david25barragan@gmail.com>
2025-10-17 20:24:35 -0500

    carpeta cliente

---
+++ src/main/java/com/project/super_extra/super_extra/cliente/cliente.java
@@ -0,0 +1,18 @@
+package com.project.super_extra.super_extra.cliente;
+
+public class cliente {
+
+    
+    
+    public static void main(String[] args) {
+        
+                    String cliente = "David Barragan";
+                    
+                    System.out.println(cliente);
+        }
+        
+
+    
+    
+    
+}
