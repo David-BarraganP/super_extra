b3491a9b1b7120f67eebcddfa7d34c94e45109ae
David-BarraganP <david25barragan@gmail.com>
2025-10-17 19:34:36 -0500

    primer commit

---
+++ src/main/java/com/project/super_extra/super_extra/SuperExtraApplication.java
@@ -0,0 +1,13 @@
+package com.project.super_extra.super_extra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperExtraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperExtraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/super_extra/super_extra/SuperExtraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.super_extra.super_extra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperExtraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
