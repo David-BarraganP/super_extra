218b877bcee7e13b2de5c8a5c3763c58500ef219
Edison Torres <edisontorres244@gmail.com>
2025-10-17 20:27:07 -0500

    Carpeta Ventas

---
+++ src/main/java/com/project/super_extra/super_extra/venta.java
@@ -0,0 +1,17 @@
+package com.project.super_extra.super_extra;
+
+public class venta {
+
+    public static void main(String[] args) {
+        
+        string venta = "Total de las ventas ";
+
+        System.out.println(venta);
+
+    }
+    
+
+}
+
+
+
